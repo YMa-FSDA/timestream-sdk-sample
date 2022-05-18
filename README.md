b828fc24b32c057a3d4142bcdc97648f67afb405
Yichao <104566011+YMa-FSDA@users.noreply.github.com>
2022-05-18 11:20:06 +0100

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Getting Started
+
+This sample is merely for the purpose of demonstration of how aws timestream sdk works.
+
+Download [Node.js](https://nodejs.org/en/). This sample is based on version 16.15.0 LTS.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm install`
+
+to install necessary packages and dependencies
+
+Then, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+Check the comments in the codes and console.
+
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import { TimestreamQuery, QueryCommand } from "@aws-sdk/client-timestream-query"; // ES Modules import
+import React from 'react'
+
+function App() {
+
+  // basic config for the query client
+
+  // NOTE: please replace the credentials with the ones in the .csv file for solely demonstration purpose. the credentials should be associated with the authorised users. we could discussed how
+  //       to implement this feature afterwards. 
+
+  const config = {
+    'region': 'eu-west-1',
+    'credentials': {
+      'accessKeyId': 'ACCESSKEYID', 'secretAccessKey': 'SECRETACCESSKEY'
+    }
+  }
+
+  // setup the client with config
+
+  const queryClient = new TimestreamQuery(config);
+
+  // basic query to get data from the db
+
+  const query1 = 'select * from "CF_Demo_DataBase"."myTestDemo" limit 2';
+
+  // main function to retrieve data 
+  // NOTE: becasue the response will return the nextToken, the function need to be called multiple times to get all data rows until the nextToken is null.
+
+  const getAllRows = async (query, nextToken = undefined) => {
+    const params = {
+      QueryString: query
+    };
+
+    if (nextToken) {
+      params.NextToken = nextToken;
+    }
+    const command = new QueryCommand(params);
+
+    queryClient.send(command)
+      .then(
+        (response) => {
+          parseQueryResult(response);
+          if (response.NextToken) {
+            getAllRows(query, response.NextToken);
+          }
+        },
+        (err) => {
+          console.error("Error while querying:", err);
+        });
+  }
+
+  // this is the function to display the data in the console. 
+
+  const parseQueryResult = (response) => {
+    console.log("Current query status: " + JSON.stringify(response.QueryStatus));
+    console.log(response);
+  }
+
+  // call the function with the desired query.
+
+  getAllRows(query1);
+
+  return (
+    <>
+      Check the response from the server in the console. Basically, the response comprises of ColumnInfo and Rows which requires
+      postprocess to clean up the data.
+    </>
+  );
+}
+
+export default App;
